860988d2b0153663239eda30f8da17a8083c2311
Tiago Miranda <ti.agodev@outlook.com>
2024-09-18 08:39:20 -0300

    map pesquisa

---
+++ collections/java-api/src/map/pesquisa/produtos/EstoqueProdutos.java
@@ -0,0 +1,42 @@
+package map.pesquisa.produtos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EstoqueProdutos {
+    private Map<Long, Produto > estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos() {
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+        double valorTotalEstoque = 0;
+        if(!estoqueProdutosMap.isEmpty()) {
+            for(Produto p : estoqueProdutosMap.values()) {
+                valorTotalEstoque += p.getPreco()* p.getQuantidade();
+            }
+        }
+        return calcularValorTotalEstoque();
+    }
+
+    public Produto obterProdutoMaisCaro() {
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        for(Produto p : estoqueProdutosMap.values()) {
+            if(p.getPreco() > maiorPreco) {
+                produtoMaisCaro = p;
+            }
+        }
+        return produtoMaisCaro;
+
+    }
+}
