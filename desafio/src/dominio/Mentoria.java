2534978a9761a81c0d8f149ee9eff2e0b692f264
Tiago Miranda <ti.agodev@outlook.com>
2024-09-19 14:04:01 -0300

    desafio java poo

---
+++ desafio/src/dominio/Mentoria.java
@@ -0,0 +1,31 @@
+package dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+
+    private LocalDate data;
+
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "\ntitulo='" + getTitulo() + '\'' +
+                "\ndescricao='" + getDescricao() + '\'' +
+                "\ndata=" + data +
+                '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20;
+    }
+}
